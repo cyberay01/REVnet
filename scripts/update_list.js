d877448b38d8f02da6441a15aec27134d502d73e
cyberay01 <ray.sakanoue@gmail.com>
2018-07-31 16:07:47 -0700

    change function name in update_list.js

--- scripts/update_list.js
+++ scripts/update_list.js
@@ -11,8 +11,8 @@ function new_element(tag_name, attributes, children=[]){
   return el;
 }
 
-function insert_meme(desc){
-  let new_li = new_element('div', {'class': 'location'}, [
+function insert_location(desc){
+  let new_div = new_element('div', {'class': 'location'}, [
     new_element('text', {'class': 'host_name'}, [document.createTextNode(desc['host_name'])]),
     new_element('br'),
     new_element('text', {'class': 'address'}, [document.createTextNode(desc['address'])]),
@@ -22,19 +22,19 @@ function insert_meme(desc){
     new_element('br')
   ]);
   let container = document.querySelector("#loc_list");
-  container.insertBefore(new_li, container.children[0]);
+  container.insertBefore(new_div, container.children[0]);
 }
 
-function refresh_memes() {
+function refresh_locations() {
   fetch('/updated_list?since=' + last_refresh.getTime()/1000, {'credentials': 'include'} )
     .then((data) => {return data.json();})
     .then((json) => {
       for (let i in json) {
-        insert_meme(json[i]);
+        insert_location(json[i]);
       }
     });
   last_refresh = new Date();
 }
 
 
-setInterval(refresh_memes, 3000);
+setInterval(refresh_locations, 3000);
