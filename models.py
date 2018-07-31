364771a0d4d9aeee8c10978064d2648d2be1f5d8
cyberay01 <ray.sakanoue@gmail.com>
2018-07-31 10:25:48 -0700

    changes to models.py and mp.html

--- models.py
+++ models.py
@@ -1,10 +1,10 @@
 from google.appengine.ext import ndb
 
 class Locations(ndb.Model):
-    host_name = ndb.StringProperty(required=False)
-    street_name1 = ndb.StringProperty(required=False)
-    street_name2 = ndb.StringProperty(required=False)
+    host_name = ndb.StringProperty(required=True)
+    address = ndb.StringProperty(required=True)
     comment = ndb.StringProperty(required=False)
-    #longitude = ndb.FloatProperty(required=True)
-    #latitude = ndb.FloatProperty(required=True)
+    longitude = ndb.FloatProperty(required=True)
+    latitude = ndb.FloatProperty(required=True)
+    created_at = ndb.DateTimeProperty(required=True)
 
