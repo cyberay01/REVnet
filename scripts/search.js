e5fa374e2d390d808ccaef3fda0da1c663ffabb1
Elly Choi <ellychoi2000@gmail.com>
2018-07-31 15:33:56 -0700

    compelted map design

--- scripts/search.js
+++ scripts/search.js
@@ -59,7 +59,7 @@ function initMap() {
         size: new google.maps.Size(71, 71),
         origin: new google.maps.Point(0, 0),
         anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(25, 25)
+        scaledSize: new google.maps.Size(50, 50)
       };
 
       // Create a marker for each place.
